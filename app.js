0a74ce674411db05ac1c5ceb7d2f68760717d428
Sandy Beyer <sandybeyer7@gmail.com>
2020-12-04 14:29:07 -0500

    fixed route issue missing /api in router path

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 dotenv.config();
 //var models = require('./models');
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECT,
-     {useNewUrlParser: true},
-     {useCreateIndex: true},
-     {useUnifiedTopology: true}
-     );
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  { useCreateIndex: true },
+  { useUnifiedTopology: true }
+);
 
 //Import my route files
-var usersRouter = require('./routes/users');
-var ordersRouter = require('./routes/orders');
-var authRouter = require('./routes/auth');
-var productsRouter = require('./routes/products');
+var usersRouter = require("./routes/users");
+var ordersRouter = require("./routes/orders");
+var authRouter = require("./routes/auth");
+var productsRouter = require("./routes/products");
 
 //jJkyB13spuxMLIjE
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-     res.send("Hello");
-})
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
 
 //Second part of my routes addition
-app.use('/users', usersRouter);
-app.use('/orders',ordersRouter);
-app.use('/auth', authRouter);
-app.use('/products', productsRouter);
+app.use("/auth", authRouter);
+
+app.use("/api/users", usersRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/products", productsRouter);
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-     console.log("We are in!");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("We are in!");
 });
 
 module.exports = app;
